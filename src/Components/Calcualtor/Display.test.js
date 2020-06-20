a572f39abd3ca73a3a0b1760cd3920cd079ae1c6
Thomas Surgent <surgentt@gmail.com>
2020-06-20 09:44:53 -0400

    Polish tests should be wrapped in describe and it block

--- src/Components/Calcualtor/Display.test.js
+++ src/Components/Calcualtor/Display.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Display from './Display';
 
-test('renders the an input from props', () => {
-  const input = '1 + 2'
-  const { getByText } = render(<Display input={input}/>);
-  const inputText = getByText(input);
-  expect(inputText).toBeInTheDocument();
+describe('Display', () => {
+  it('should render the an input from props', () => {
+    const input = '1 + 2'
+    const {getByText} = render(<Display input={input}/>);
+    const inputText = getByText(input);
+    expect(inputText).toBeInTheDocument();
+  });
 });
--- src/Components/Shared/Button.test.js
+++ src/Components/Shared/Button.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Button from './Button';
 
-test('renders a button', () => {
-  const { getByText } = render(<Button handleClick={() => {} }>Hello</Button>);
-  const buttonText = getByText(/Hello/i);
-  expect(buttonText).toBeInTheDocument();
+describe('Button', () => {
+  it('should render a button', () => {
+    const {getByText} = render(<Button handleClick={() => {
+    }}>Hello</Button>);
+    const buttonText = getByText(/Hello/i);
+    expect(buttonText).toBeInTheDocument();
+  });
 });
